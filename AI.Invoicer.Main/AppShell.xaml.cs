e411f7f9f6fedbb22946e91cdcda22fca2f8a40d
BUSH <keevbush@hotmail.com>
2025-11-24 09:29:03 +0200

    Ajoutez des fichiers projet.

---
+++ AI.Invoicer.Main/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AI.Invoicer.Main
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AI.Invoicer.Main/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AI.Invoicer.Main
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
